c52193bcaf8f539e759044fb879d83d6fa2ecfa9
mohammed-chirah <m.chirah@bizops.ma>
2026-03-08 18:17:43 +0000

    Add GetBookingByIdQuery record for retrieving booking details by ID

---
+++ FootCourtBooking.Application/Bookings/GetBookingById/GetBookingByIdQuery.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootCourtBooking.Application.Bookings.GetBookingById
+{
+   public sealed record GetBookingByIdQuery(Guid Id) ;
+}
