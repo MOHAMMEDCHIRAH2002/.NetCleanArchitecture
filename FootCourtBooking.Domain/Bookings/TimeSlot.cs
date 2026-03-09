bf6d52ffc6f0da0db987c60143470dde6fd3d84c
mohammed-chirah <m.chirah@bizops.ma>
2026-03-09 14:56:39 +0000

    Refactor TimeSlot class to enforce booking duration and time constraints; add AppDbContext for database context management

--- FootCourtBooking.Domain/Bookings/TimeSlot.cs
+++ FootCourtBooking.Domain/Bookings/TimeSlot.cs
@@ -8,33 +8,37 @@ namespace FootCourtBooking.Domain.Bookings
 {
     public sealed class TimeSlot
     {
-        public DateTime Start  {get;}
-        public DateTime End {get;}
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
 
+        public TimeSlot()
+        {
+
+        }
         public TimeSlot(DateTime start, DateTime end)
         {
-            if(end <= start)
-            
-                throw new DomainException("End time must be greater than start time");
-            
-           
-           if(start.Minute !=0 || start.Second!=0 || start.Millisecond !=0)
-              throw new DomainException("Start time must be on the hour");
+            if (end <= start)
 
-           if(end.Minute!=0 || end.Second!=0 || end.Millisecond !=0)
-              throw new DomainException("End time must be on the hour");
-            
-            if((end-start).TotalHours !=1)
+                throw new DomainException("End time must be greater than start time");
+
+
+            if (start.Minute != 0 || start.Second != 0 || start.Millisecond != 0)
+                throw new DomainException("Start time must be on the hour");
+
+            if (end.Minute != 0 || end.Second != 0 || end.Millisecond != 0)
+                throw new DomainException("End time must be on the hour");
+
+            if ((end - start).TotalHours != 1)
                 throw new DomainException("Booking durration must be exactly 1 hour");
             Start = start;
             End = end;
-              
-        } 
+
+        }
 
         public bool Overlaps(TimeSlot other)
         {
             return Start < other.End && End > other.Start;
-        }    
-        
+        }
+
     }
 }
---
+++ FootCourtBooking.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootCourtBooking.Infrastructure.Persistence
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
